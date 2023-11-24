358623ec1a55ba3cc097d6cd07c607221acf0b9b
TahmidShawn7 <tahmidshawn7@gmail.com>
2023-11-24 12:42:30 +0600

    banner section slider setup completed

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
     const navLinks = <>
@@ -7,6 +8,7 @@ const Navbar = () => {
         <NavLink>Trainer</NavLink>
         <NavLink>Classes</NavLink>
         <NavLink to='dashboard'>Dashboard</NavLink>
+        <NavLink>Community</NavLink>
     </>
     return (
         <nav className="navbar bg-[#554640] text-white px-3">
@@ -14,10 +16,10 @@ const Navbar = () => {
                 <button className="text-3xl font-semibold px-8 py-2 mr-8 hidden lg:inline">ZenFiT</button>
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <FiMenu className="text-2xl" />
                     </label>
 
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-black">
                         {navLinks}
                     </ul>
                 </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root/Root";
 import Home from "../pages/Home/Home";
+import Error from "../pages/Error/Error";
 
 
 const Routes = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <Error></Error>,
         children: [
             {
                 path: '/',
