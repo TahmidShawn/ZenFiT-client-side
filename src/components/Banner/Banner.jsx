cba3c0003ed7368ad139cea111658c101e4ef595
TahmidShawn7 <tahmidshawn7@gmail.com>
2023-11-24 15:39:51 +0600

    sider img added but design still left

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,46 +1,104 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+import slider from './../../assets/slider/slider.jpg';
+import slider1 from './../../assets/slider/slider1.jpg';
+import slider2 from './../../assets/slider/slider2.jpg';
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Banner = () => {
     return (
-        <Carousel>
-            <div>
-                <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                    <div className="hero-overlay bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-md">
-                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                            <button className="btn btn-primary">Get Started</button>
-                        </div>
+        <AutoplaySlider
+            className='h-[600px]'
+            play={true}
+            cancelOnInteraction={false}
+            interval={3000}
+            infinite={true}
+            buttons={false}
+        >
+            <div
+                className="hero min-h-full"
+                style={{
+                    backgroundImage: `url(${slider1})`,
+                    backgroundSize: 'cover', // Ensure the image covers the entire slider
+                    backgroundPosition: 'center',
+                }}
+            >
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md text-white">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                            deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
-            <div>
-                <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                    <div className="hero-overlay bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-md">
-                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                            <button className="btn btn-primary">Get Started</button>
-                        </div>
+            <div
+                className="hero min-h-full"
+                style={{
+                    backgroundImage: `url(${slider2})`,
+                    backgroundSize: 'cover', 
+                    backgroundPosition: 'center',
+                }}
+            >
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md text-white">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                            deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
-            <div>
-                <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                    <div className="hero-overlay bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-md">
-                            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                            <button className="btn btn-primary">Get Started</button>
-                        </div>
+            <div
+                className="hero min-h-full"
+                style={{
+                    backgroundImage: `url(${slider})`,
+                    backgroundSize: 'cover', 
+                    backgroundPosition: 'center',
+                }}
+            >
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md text-white">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                            deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
-        </Carousel>
+
+            <div
+                className="hero min-h-full"
+                style={{
+                    backgroundImage: `url(${slider2})`,
+                    backgroundSize: 'cover', 
+                    backgroundPosition: 'center',
+                }}
+            >
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md text-white">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
+                            deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </AutoplaySlider>
     );
 };
 
